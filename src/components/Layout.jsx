13b5ebbf97231a4f0339d1b0a692e96c765ad864
Ivan Klimov <klimov128@gmail.com>
2023-06-18 14:32:14 +0300

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import { AppBar } from './Header/AppBar';
+import { Header } from './Header/Header';
 import { Suspense } from 'react';
 
 export const Layout = () => {
   return (
     <div style={{ maxWidth: '600px', margin: '40px auto', padding: '0 10px' }}>
-      <AppBar />
+      <Header />
       <Suspense>
         <Outlet />
       </Suspense>
