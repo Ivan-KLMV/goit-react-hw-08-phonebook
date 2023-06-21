e0739f5c70fa2ce23ed0394cf98507037a44bc5b
Ivan Klimov <klimov128@gmail.com>
2023-06-21 22:41:05 +0300

    Revert "Update Layout.jsx"
    This reverts commit 13b5ebbf97231a4f0339d1b0a692e96c765ad864.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import { Header } from './Header/Header';
+import { AppBar } from './Header/AppBar';
 import { Suspense } from 'react';
 
 export const Layout = () => {
   return (
     <div style={{ maxWidth: '600px', margin: '40px auto', padding: '0 10px' }}>
-      <Header />
+      <AppBar />
       <Suspense>
         <Outlet />
       </Suspense>
